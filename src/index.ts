1b2aff9ff5383af791c9995332431c8e98fab821
CodingPyNumbers <78205492+CodingPyNumbers@users.noreply.github.com>
2025-03-05 16:30:02 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export default {
   async fetch(request, env) {
     const inputs = {
-      prompt: "Rat on a motorcycle on the beach",
+      prompt: "",
     };
 
     const response = await env.AI.run(
